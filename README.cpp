1fef07165f51d0fec5e0bcf56d72a5665d72b5c8
rav0215 <turuntzeva.anastasiya@yandex.ru>
2016-10-07 09:42:35 +0300

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -1,6 +1,43 @@
+/*////////////////////
+ Турунцева Анастасия//
+ ИУ8-14///////////////
+ *////////////////////
+/* Решение показательного уравнения
+ */
+
 #include <iostream>
-#include <math>
+#include <math.h>
 using namespace std;
-int main()                                                                                                     
+int main()
 {
-double 
+    double x, m;
+    cout <<"x = "<< endl;
+    cin >> x;
+         {
+        pow(3, 2x) + 4 * pow(3, x) - 5 = 0;
+    }
+    if (pow(3, x) == m )
+         {
+        pow(m, 2) + 4 * m - 5 = 0;
+    }
+    double D, m1, m2;
+    D = 4*4 - 4*1*(-5);
+    cout << "Дискриминант равен\n" << D << endl;
+    if (D>0)
+    {
+        m1 = ( -4 + sqrt(D))/(2*1);
+        m2 = ( -4 - sqrt(D))/(2*1);
+        cout << "Первый корень равен\n" << m1 << endl;
+        cout << "\n";
+        cout << "Второй корень равен\n" << m2 << endl;
+    }
+    if ((m1>0)&(m2<0))
+    {
+        pow(3, x) = m1;
+        pow(3, x) = m2;// корней нет
+        x = log3(x);
+        
+    }
+
+    return 0;
+}
