2da3a2ba0e8eea82476f55ffe328fba8aa0e442a
rav0215 <turuntzeva.anastasiya@yandex.ru>
2016-10-21 00:55:34 +0300

    Update Task2.cpp

--- Task2.cpp
+++ Task2.cpp
@@ -1,42 +1,46 @@
-#include <iostream>
-#include <math.h>
-
-
-int main() {
-    double pi;
-    int f; 
-    float a, h, r, d1, d2, S; 
-    cout <<"the definition of figure type";
-    cout <<"1 - parallelogram, 2 - sphere, 3 - rhomb";
-    cin >> f;
-    if ((f<1)&(f>3))
-        cout << "programming error";
-    else {
-        switch (f) {
-            case 1 :
-                cout << "enter the value of the side of parallelogram";
-                cin >> a;
-                cout << "enter the height value of parallelogram";
-                cin >> h;
-                S = a * h;
-                break;
-            case 2 :
-                cout << "enter the radius of the sphere";
-                cin >> r;
-                S = 4 * pi * pow(r, 2);
-                break;
-            case 3 :
-                cout << "enter the value of the first diagonal of rhomb";
-                cin >> d1;
-                cout << "enter the value of the second diagonal of rhomb";
-                cin >> d2;
-                S = (d1 * d2) / 2;
-                break;
-
-        }
-
-        cout << "the square preset shape equal" << S;
+	#include <iostream>
+	 
+	using namespace std;
+	 
+	void playgame()
+	{
+	cout << "Play game called";
+	}
+	void loadgame()
+	{
+	cout << "Load game called";
     }
-    
-    return 0;
-}
+    void playmultiplayer()
+	{
+	cout << "Play multiplayer game called";
+	}
+	 	int main()
+	{
+	int input;
+    cout << "1. Play game\n";
+	cout << "2. Load game\n";
+	cout << "3. Play multiplayer\n";
+	cout << "4. Exit\n";
+	cout << "Selection: ";
+	cin >> input;
+	switch ( input ) {
+	case 1:
+	playgame ();
+	break;
+    case 2:
+	loadgame ();
+	break;
+	case 3:
+	playmultiplayer ();
+	break;
+	case 4:
+	cout << "Thank you for playing!\n";
+	break;
+    default:
+	cout << "Error, bad input, quitting\n";
+    break;
+	}
+	return 0;
+	}
+
+
